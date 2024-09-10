8141761a357c10ec916d1f8cb8504646e7d55618
sbbrl <git@subibhu.info.np>
2024-09-10 11:03:03 +0000

    Corrections

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('home/',views.home,name="home"),
     path('educative/',views.educative,name="educative"),
-    path('hello/<str:name>/',views.hello,name="hello")
+    path('hello/<str:name>/',views.hello,name="hello"),
+    path('eod/<int:num>/',views.eod,name="eod")
     ]
